23d59b2e9ef9c6b2ce16b15f8c5a1505f11206a4
André R. Bronca <bronca.andre@gmail.com>
2024-03-06 11:45:46 -0300

    Criando formulário

---
+++ beginn_php8_mysql/capitulo05/basicForm.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="functions.php" method="post">
+        Name: <input type="text" id="name" name="name"><br>
+        E-mail: <input type="email" name="email" id="email"><br>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
---
+++ beginn_php8_mysql/capitulo05/functions.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Thank you <h1>{$_POST['name']}</h1>. 
+    I will email you at {$_POST['email']}";
+?>
+<br>
+<a href="basicForm.php">voltar</a>
