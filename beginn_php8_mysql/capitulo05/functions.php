07ec3f1d0a12d965a2fefac866c7ee3b1f065b6d
André R. Bronca <bronca.andre@gmail.com>
2024-03-06 11:51:42 -0300

    tratando a entrada de dados do formulário.

--- beginn_php8_mysql/capitulo05/functions.php
+++ beginn_php8_mysql/capitulo05/functions.php
@@ -1,7 +1,12 @@
 <?php
+$name = filter_var($_POST['name'], 
+    FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-echo "Thank you <h1>{$_POST['name']}</h1>. 
-    I will email you at {$_POST['email']}";
+$email = filter_var($_POST['email'], 
+    FILTER_SANITIZE_EMAIL);
+
+echo "Thank you {$name}. 
+    I will email you at {$email}";
 ?>
 <br>
 <a href="basicForm.php">voltar</a>
