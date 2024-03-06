7c25c3f5eb2309268be84f1428a970cef3f3a592
André R. Bronca <bronca.andre@gmail.com>
2024-03-06 11:55:00 -0300

    tratando a entrada de e-mail no form

--- beginn_php8_mysql/capitulo05/functions.php
+++ beginn_php8_mysql/capitulo05/functions.php
@@ -1,12 +1,22 @@
 <?php
+$emailErr = null;
 $name = filter_var($_POST['name'], 
     FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
 $email = filter_var($_POST['email'], 
     FILTER_SANITIZE_EMAIL);
 
-echo "Thank you {$name}. 
-    I will email you at {$email}";
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    $emailErr = "Formato inválildo do email";
+}
+
+if (!$emailErr){
+    echo "Thank you {$name}. I will email you at {$email}";
+} else {
+    echo $emailErr;
+}
+
+
 ?>
 <br>
 <a href="basicForm.php">voltar</a>
